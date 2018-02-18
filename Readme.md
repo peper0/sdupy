09012324f2cae63c46b8595e4f34431a3e7e815f
Tomasz Łakota <tomasz.lakota@gmail.com>
2018-02-18 21:58:30 +0100

    Bleble

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# SduPy - Simple Declarative Ui for Python
+
+("Simple" may be changed into "Sophisticated" in the future without renaming anything).
--- helpers.py
+++ helpers.py
@@ -9,13 +9,25 @@ from .widgets import Plot
 
 
 @reactive_finalizable()
-def show_image(widget_name: str, image, use_bgr=True):
+def imshow(widget_name: str, image: np.ndarray, use_bgr=True):
+    """
+    :param widget_name: Unique identifier among all widgets. If such widget doesn't exist, it will be created.
+    :param image: Any image that matplotlib can plot with imshow.
+    :param use_bgr: If the image has 3 components, treat them as Blue, Green, Red in this order (Red Green Blue
+                    otherwise)
+    :param keep: Keep the image even when no reference to the returned object is kept. Only the last one is kept for
+                 each widget.
+    :return:
+    """
     assert isinstance(widget_name, str)
     plot = studioapp.default_main_window.obtain_widget(widget_name, Plot)
-    if use_bgr and len(image.shape) == 3 and image.shape[2] == 3:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img = plot.axes.imshow(image)  # type: plt.image.AxesImage
+    axes_image = None
+    if image is not None:
+        if use_bgr and len(image.shape) == 3 and image.shape[2] == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        axes_image = plot.axes.imshow(image)  # type: plt.image.AxesImage
     plot.draw()
     yield
     os.write(1, b"removing\n")
-    img.remove()
+    if axes_image is not None:
+        axes_image.remove()
