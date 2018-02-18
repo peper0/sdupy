42dda3c4a2eef2b6be3e454e41ed5a9657a8f5f9
Tomasz Łakota <tomasz.lakota@gmail.com>
2018-02-18 23:11:42 +0100

    Plots: zooming on scroll, tight layout

--- helpers.py
+++ helpers.py
@@ -1,15 +1,19 @@
 import os
 
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 from . import studioapp
 from .reactive.reactive import reactive_finalizable
+from .reactive import Var
 from .widgets import Plot
+
+kept_references = dict()  # Dict[str, Var]
 
 
 @reactive_finalizable()
-def imshow(widget_name: str, image: np.ndarray, use_bgr=True):
+def imshow(widget_name: str, image: np.ndarray, use_bgr=True, **kwargs):
     """
     :param widget_name: Unique identifier among all widgets. If such widget doesn't exist, it will be created.
     :param image: Any image that matplotlib can plot with imshow.
@@ -22,10 +26,13 @@ def imshow(widget_name: str, image: np.ndarray, use_bgr=True):
     assert isinstance(widget_name, str)
     plot = studioapp.default_main_window.obtain_widget(widget_name, Plot)
     axes_image = None
+
     if image is not None:
+        kwargs.setdefault('aspect', 'equal')
+        kwargs.setdefault('interpolation', 'nearest')
         if use_bgr and len(image.shape) == 3 and image.shape[2] == 3:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        axes_image = plot.axes.imshow(image)  # type: plt.image.AxesImage
+        axes_image = plot.axes.imshow(image, **kwargs)  # type: plt.image.AxesImage
     plot.draw()
     yield
     os.write(1, b"removing\n")
--- widgets/plot.py
+++ widgets/plot.py
@@ -1,15 +1,16 @@
+from PyQt5 import QtGui
+
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QVBoxLayout, QWidget
-from matplotlib.backend_bases import key_press_handler
+from matplotlib.backend_bases import key_press_handler, MouseEvent
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, \
     NavigationToolbar2QT as NavigationToolbar
 
 from .common.register import register_widget
 
 
-@register_widget("matplotlib plot")
-class Plot(QWidget):
+class Axes(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.layout = QVBoxLayout(self)
@@ -25,19 +26,50 @@ class Plot(QWidget):
         self.mpl_toolbar = NavigationToolbar(self.canvas, self)
         self.layout.addWidget(self.mpl_toolbar)
         self.canvas.mpl_connect('key_press_event', self.on_key_press)
+        self.canvas.mpl_connect('scroll_event', self.on_scroll)
+        self.mpl_toolbar.pan()  # we usually want to pan with mouse, since zooming is on the scroll
 
         self.axes = self.figure.add_subplot(111)
+        self.axes.set_adjustable('datalim')  # use whole area when keeping aspect ratio of images
         self.canvas.draw()
 
-        # self.tree = QTreeView()
-        # self.tree.setModel(DataTreeModel(self.studio, self))
-        # self.layout.addWidget(self.tree)
-
     def on_key_press(self, event):
-        print('you pressed', event.key)
         # implement the default mpl key press events described at
         # http://matplotlib.org/users/navigation_toolbar.html#navigation-keyboard-shortcuts
         key_press_handler(event, self.canvas, self.mpl_toolbar)
 
+    def on_scroll(self, event: MouseEvent):
+        ax = self.axes
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        xfocus = event.xdata # get event x location
+        yfocus = event.ydata # get event y location
+        xlim_delta = [l - xfocus for l in xlim]
+        ylim_delta = [l - yfocus for l in ylim]
+
+        SCALE_PER_TICK = 1.3
+        if event.button == 'up':
+            scale_factor = 1/SCALE_PER_TICK
+        elif event.button == 'down':
+            scale_factor = SCALE_PER_TICK
+        else:
+            # deal with something that should never happen
+            scale_factor = 1
+
+        xlim_zoomed = [l*scale_factor + xfocus for l in xlim_delta]
+        ylim_zoomed = [l*scale_factor + yfocus for l in ylim_delta]
+        ax.set_xlim(xlim_zoomed)
+        ax.set_ylim(ylim_zoomed)
+        self.draw()
+
     def draw(self):
         self.canvas.draw()
+
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        self.figure.tight_layout()
+
+
+
+@register_widget("matplotlib plot")
+class Plot(Axes):
+    pass
